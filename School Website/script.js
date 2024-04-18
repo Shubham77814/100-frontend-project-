41e3dfef60d51caccdcb29a7b25e31c3d02873db
Shubham Chauhan <chauhansubham843@gmail.com>
2024-04-19 01:53:01 +0545

    add new file

---
+++ School Website/script.js
@@ -0,0 +1,7 @@
+function showMenu() {
+    document.querySelector("#nav-items").style.right = "0";
+  }
+
+  function hideMenu() {
+    document.querySelector("#nav-items").style.right = "-300px";
+  }
